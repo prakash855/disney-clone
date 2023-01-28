a5d790f0bc1388d6095e027f55eed3a3f024a586
prakash855 <sshivamprakashh301@gmail.com>
2023-01-29 00:42:44 +0530

    feat: firebase

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCYFHlysNLwpB6-ZDYk8OCyjxfsF9IGmtY",
+  authDomain: "disney-plus-cdf5c.firebaseapp.com",
+  projectId: "disney-plus-cdf5c",
+  storageBucket: "disney-plus-cdf5c.appspot.com",
+  messagingSenderId: "569715628773",
+  appId: "1:569715628773:web:400e75e0509b893751120c",
+  measurementId: "G-SRZ5TF3HDD",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+
+export { auth, provider, storage };
+export default db;
